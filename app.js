7bc1b7de58e1bf025eb6540d49978254045882e7
UsoltsevKonstantin <73591223+kostiausolicev@users.noreply.github.com>
2023-07-07 15:09:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let tg = window.Telegram.WebApp;
 
 let p = document.createElement("p");
 
-let data = tg.initDataUnsefe;
+let data = tg.initDataUnsafe;
 p.innerText = data;
 let tableDiv = document.getElementById("table_div");
 tableDiv.appendChild(p);
