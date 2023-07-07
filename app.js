8ef4aed421150c0d2b86c93686fc0b75ffd7f01c
UsoltsevKonstantin <73591223+kostiausolicev@users.noreply.github.com>
2023-07-07 14:45:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 let tg = window.Telegram.WebApp;
 
-let table = document.createElement("table");
+let p = document.createElement("p");
 
-let tHead = document.createElement("thead");
-let tBody = document.createElement("tbody");
-
+let data = tg.initData;
+p.innerText = data;
 let tableDiv = document.getElementById("table_div");
+tableDiv.appendChild(p);
 
-let data = tg.initDataUnsafe.user.id;
-
-
-tableDiv.appendChild(table);
+// let table = document.createElement("table");
+//
+// let tHead = document.createElement("thead");
+// let tBody = document.createElement("tbody");
+//
+// let tableDiv = document.getElementById("table_div");
+//
+// let data = tg.initDataUnsafe.user.id;
+//
+//
+// tableDiv.appendChild(table);
