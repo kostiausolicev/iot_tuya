64565ec97a04789eb6afcd737f6915c355950de5
kostiausolicev <kostiausolicev@gmail.com>
2023-12-08 14:54:21 +0300

    added main button

--- app.js
+++ app.js
@@ -7,3 +7,4 @@ tg.MainButton.setText("Changed Text1"); //изменяем текст кнопк
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
+tg.MainButton.show()
