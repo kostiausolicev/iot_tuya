041c40fe11772ad8df8460d5ec837d65f71a3066
kostiausolicev <kostiausolicev@gmail.com>
2023-12-08 16:40:20 +0300

    update app.js

--- app.js
+++ app.js
@@ -6,19 +6,71 @@ tg.MainButton.setText("Отправить данные");
 tg.MainButton.enable()
 tg.MainButton.show()
 
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+let formType = getParameterByName('formType');
+
+if (formType === 'auth') {
+    document.getElementById('dynamicFormContainer').innerHTML = `
+        <form id="myForm">
+             <label for="name">Username:</label>
+            <input type="text" id="name" name="name" required><br>
+
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br>
+
+            <label for="confirm_password">Repeat password:</label>
+            <input type="password" id="confirm_password" name="confirm_password" required><br>
+        </form>
+    `;
+} else if (formType === 'register') {
+    document.getElementById('dynamicFormContainer').innerHTML = `
+        <form id="myForm">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br>
+        </form>
+    `;
+}
+
+
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var confirm_password = document.getElementById('confirm password').value;
+    const name = document.getElementById('name').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const confirm_password = document.getElementById('confirm password').value;
 
-    var formData = {
-        username: username,
-        password: password,
-        confirm_password: confirm_password
-    };
-
-    // Получение JSON-представления объекта
-    var formDataJSON = JSON.stringify(formData);
+    let formDataJSON;
+    if (formType === 'auth') {
+        const formData = {
+            name: name,
+            username: username,
+            password: password,
+            confirm_password: confirm_password
+        };
+        formDataJSON = JSON.stringify(formData);
+    } else if (formType === 'register') {
+        const formData = {
+            name: name,
+            username: username,
+            password: password,
+            confirmPassword: confirm_password
+        };
+        formDataJSON = JSON.stringify(formData);
+    }
     tg.sendData("formDataJSON");
     tg.close();
 });
