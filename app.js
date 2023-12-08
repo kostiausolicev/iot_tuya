489e804908d04c417404c1355ba2a069ab69608f
kostiausolicev <kostiausolicev@gmail.com>
2023-12-08 15:07:00 +0300

    added main button

--- app.js
+++ app.js
@@ -14,3 +14,24 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
     tg.sendData("some string that we need to send");
     //при клике на основную кнопку отправляем данные в строковом виде
 });
+
+btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+    if (tg.MainButton.isVisible){ //если кнопка показана
+        tg.MainButton.hide() //скрываем кнопку
+    }
+    else{ //иначе
+        tg.MainButton.show() //показываем
+    }
+});
+
+let btnED = document.getElementById("btnED"); //получаем кнопку активировать/деактивировать
+btnED.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+    if (tg.MainButton.isActive){ //если кнопка показана
+        tg.MainButton.setParams({"color": "#E0FFFF"}); //меняем цвет
+        tg.MainButton.disable() //скрываем кнопку
+    }
+    else{ //иначе
+        tg.MainButton.setParams({"color": "#143F6B"}); //меняем цвет
+        tg.MainButton.enable() //показываем
+    }
+});
