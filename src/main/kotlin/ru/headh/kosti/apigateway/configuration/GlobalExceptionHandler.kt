2937d67dd6c3f42e51a146073078af8150b55d62
kostiausolicev <kostiausolicev@gmail.com>
2023-12-08 16:30:11 +0300

    updated exception handler

--- src/main/kotlin/ru/headh/kosti/apigateway/configuration/GlobalExceptionHandler.kt
+++ src/main/kotlin/ru/headh/kosti/apigateway/configuration/GlobalExceptionHandler.kt
@@ -1,15 +1,29 @@
 package ru.headh.kosti.apigateway.configuration
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseBody
+import ru.headh.kosti.apigateway.dto.ExceptionDto
 
 @ControllerAdvice
 class GlobalExceptionHandler {
+    private val mapper = jacksonObjectMapper()
     @ExceptionHandler(Exception::class)
-    fun handle(e: Exception): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.message)
+    fun handle(exception: Exception): ResponseEntity<Any> {
+        val ex = exception.message
+            ?: return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ExceptionDto("Ошибка сервера", "SERVER_ERROR"))
+        val code = ex.substring(0, 3).toIntOrNull()
+        try {
+            val message = ex.substring(7, ex.length - 1)
+            val dto: ExceptionDto = mapper.readValue(message)
+            return ResponseEntity.status(code!!).body(dto)
+        } catch (_: Exception) {
+            return ResponseEntity.status(code!!).body("Что-то произошло")
+        }
     }
 }
