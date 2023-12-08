223ec47bf529d9e0d16229d3708693d663daeaf5
kostiausolicev <kostiausolicev@gmail.com>
2023-12-08 15:25:46 +0300

    added main button

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 
 tg.expand(); //расширяем на все окно
 
-tg.MainButton.setText("Changed Text");
+tg.MainButton.setText("Отправить данные");
 tg.MainButton.enable()
 tg.MainButton.show()
 
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
     tg.sendData("some string that we need to send");
+    tg.close();
 });
