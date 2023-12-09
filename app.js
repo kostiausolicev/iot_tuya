637503b838555b594277450c33c5bb15ed123c22
kostiausolicev <kostiausolicev@gmail.com>
2023-12-10 02:46:12 +0300

    updated app.js

--- app.js
+++ app.js
@@ -176,7 +176,7 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
         if (document.getElementById('switch_led_check').checked) {
             formData.push({
                 code: 'SWITCH_LED',
-                value: switch_led === "true"
+                value: switch_led
             });
         }
 
