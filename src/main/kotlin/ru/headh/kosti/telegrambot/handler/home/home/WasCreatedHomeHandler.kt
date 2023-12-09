71ba3fdfd136a00685a98b6cfb91da7351f121f4
kostiausolicev <kostiausolicev@gmail.com>
2023-12-10 00:32:16 +0300

    added creating of home and room

---
+++ src/main/kotlin/ru/headh/kosti/telegrambot/handler/home/home/WasCreatedHomeHandler.kt
@@ -0,0 +1,60 @@
+package ru.headh.kosti.telegrambot.handler.home.home
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+import ru.headh.kosti.apigateway.client.model.HomeRequestGenGen
+import ru.headh.kosti.telegrambot.aspect.CheckAndUpdateToken
+import ru.headh.kosti.telegrambot.client.HomeServiceClient
+import ru.headh.kosti.telegrambot.dto.home.home.WasCreatedHomeActionData
+import ru.headh.kosti.telegrambot.enumeration.ActionType
+import ru.headh.kosti.telegrambot.handler.ActionHandler
+import ru.headh.kosti.telegrambot.repository.RedisRepository
+import ru.headh.kosti.telegrambot.sender.TelegramSender
+import ru.headh.kosti.telegrambot.util.MAIN_MENU
+
+@Component
+class WasCreatedHomeHandler(
+    private val telegramSender: TelegramSender,
+    private val homeServiceClient: HomeServiceClient,
+    private val redisRepository: RedisRepository
+) : ActionHandler<WasCreatedHomeActionData> {
+    override val type: ActionType = ActionType.WAS_CREATED_HOME
+
+    private val mapper = jacksonObjectMapper()
+
+    @CheckAndUpdateToken
+    override fun handle(data: WasCreatedHomeActionData) {
+        val home: Map<String, Any> = mapper.readValue(data.message)
+        val tokens = redisRepository.findByIdOrNull(data.chatId)!!
+        homeServiceClient.createHome("Bearer ${tokens.accessToken}", home.let {
+            HomeRequestGenGen(
+                name = it["name"].toString(),
+                address = if (it["address"].toString() == "") null else it["room_id"].toString(),
+            )
+        })
+        telegramSender.sendMessage(
+            chatId = data.chatId,
+            text = "wait...",
+            replyKeyboardRemove = ReplyKeyboardRemove(true)
+        ).also { telegramSender.deleteMessage(chatId = data.chatId, messageId = it.messageId) }
+        telegramSender.sendMessage(
+            chatId = data.chatId,
+            text = "Вы создали новый дом!",
+            inlineReplyMarkup = InlineKeyboardMarkup(
+                listOf(
+                    listOf(
+                        InlineKeyboardButton.builder()
+                            .text("В главное меню")
+                            .callbackData(MAIN_MENU)
+                            .build()
+                    )
+                )
+            )
+        )
+    }
+}
---
+++ src/main/kotlin/ru/headh/kosti/telegrambot/handler/home/room/WasCreatedRoomHandler.kt
@@ -0,0 +1,65 @@
+package ru.headh.kosti.telegrambot.handler.home.room
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+import ru.headh.kosti.apigateway.client.model.HomeRequestGenGen
+import ru.headh.kosti.apigateway.client.model.RoomRequestGenGen
+import ru.headh.kosti.telegrambot.aspect.CheckAndUpdateToken
+import ru.headh.kosti.telegrambot.client.HomeServiceClient
+import ru.headh.kosti.telegrambot.client.RoomServiceClient
+import ru.headh.kosti.telegrambot.dto.home.home.WasCreatedHomeActionData
+import ru.headh.kosti.telegrambot.dto.home.room.WasCreatedRoomActionData
+import ru.headh.kosti.telegrambot.enumeration.ActionType
+import ru.headh.kosti.telegrambot.handler.ActionHandler
+import ru.headh.kosti.telegrambot.repository.RedisRepository
+import ru.headh.kosti.telegrambot.sender.TelegramSender
+import ru.headh.kosti.telegrambot.util.MAIN_MENU
+
+@Component
+class WasCreatedRoomHandler(
+    private val telegramSender: TelegramSender,
+    private val roomServiceClient: RoomServiceClient,
+    private val redisRepository: RedisRepository
+) : ActionHandler<WasCreatedRoomActionData> {
+    override val type: ActionType = ActionType.WAS_CREATED_ROOM
+
+    private val mapper = jacksonObjectMapper()
+
+    @CheckAndUpdateToken
+    override fun handle(data: WasCreatedRoomActionData) {
+        val room: Map<String, Any> = mapper.readValue(data.message)
+        val tokens = redisRepository.findByIdOrNull(data.chatId)!!
+        roomServiceClient.createRoom(
+            authorization = "Bearer ${tokens.accessToken}",
+            homeId = room["home_id"].toString().toInt(),
+            roomRequestGenGen = room.let {
+                RoomRequestGenGen(
+                    name = it["name"].toString(),
+                )
+            })
+        telegramSender.sendMessage(
+            chatId = data.chatId,
+            text = "wait...",
+            replyKeyboardRemove = ReplyKeyboardRemove(true)
+        ).also { telegramSender.deleteMessage(chatId = data.chatId, messageId = it.messageId) }
+        telegramSender.sendMessage(
+            chatId = data.chatId,
+            text = "Вы создали новый дом!",
+            inlineReplyMarkup = InlineKeyboardMarkup(
+                listOf(
+                    listOf(
+                        InlineKeyboardButton.builder()
+                            .text("В главное меню")
+                            .callbackData(MAIN_MENU)
+                            .build()
+                    )
+                )
+            )
+        )
+    }
+}
