4d789865c32bdf9caf954eae90f60be9816a4c0b
kostiausolicev <kostiausolicev@gmail.com>
2023-12-09 19:13:51 +0300

    added deleting of room and device

--- src/main/kotlin/ru/headh/kosti/telegrambot/handler/device/DeleteDeviceHandler.kt
+++ src/main/kotlin/ru/headh/kosti/telegrambot/handler/device/DeleteDeviceHandler.kt
@@ -1,7 +1,41 @@
 package ru.headh.kosti.telegrambot.handler.device
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+import ru.headh.kosti.telegrambot.aspect.CheckAndUpdateToken
+import ru.headh.kosti.telegrambot.client.DeviceServiceClient
+import ru.headh.kosti.telegrambot.dto.device.DeleteDeviceActionData
+import ru.headh.kosti.telegrambot.dto.home.home.DeleteHomeActionData
+import ru.headh.kosti.telegrambot.enumeration.ActionType
+import ru.headh.kosti.telegrambot.handler.ActionHandler
+import ru.headh.kosti.telegrambot.repository.RedisRepository
+import ru.headh.kosti.telegrambot.sender.TelegramSender
+import ru.headh.kosti.telegrambot.util.GET_DEVICE_LIST
 
 @Component
-class DeleteDeviceHandler {
+class DeleteDeviceHandler(
+    private val telegramSender: TelegramSender,
+    private val deviceServiceClient: DeviceServiceClient,
+    private val redisRepository: RedisRepository
+) : ActionHandler<DeleteDeviceActionData> {
+    override val type: ActionType = ActionType.DELETE_DEVICE
+
+    @CheckAndUpdateToken
+    override fun handle(data: DeleteDeviceActionData) {
+        val deviceId = data.message.split(":")[1].toInt()
+        redisRepository.findByIdOrNull(data.chatId)
+            ?.let { deviceServiceClient.delete1("Bearer ${it.accessToken}", deviceId) }
+        telegramSender.editMessage(
+            chatId = data.chatId,
+            messageId = data.messageId,
+            text = "Ваше устройство удалено",
+            inlineReplyMarkup = InlineKeyboardMarkup(listOf(listOf(
+                InlineKeyboardButton.builder()
+                .text("Вернуться назад")
+                .callbackData(GET_DEVICE_LIST)
+                .build())))
+        )
+    }
 }
--- src/main/kotlin/ru/headh/kosti/telegrambot/handler/home/room/DeleteRoomHandler.kt
+++ src/main/kotlin/ru/headh/kosti/telegrambot/handler/home/room/DeleteRoomHandler.kt
@@ -1,18 +1,40 @@
 package ru.headh.kosti.telegrambot.handler.home.room
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+import ru.headh.kosti.telegrambot.aspect.CheckAndUpdateToken
+import ru.headh.kosti.telegrambot.client.RoomServiceClient
 import ru.headh.kosti.telegrambot.dto.home.room.DeleteRoomActionData
 import ru.headh.kosti.telegrambot.enumeration.ActionType
 import ru.headh.kosti.telegrambot.handler.ActionHandler
+import ru.headh.kosti.telegrambot.repository.RedisRepository
 import ru.headh.kosti.telegrambot.sender.TelegramSender
+import ru.headh.kosti.telegrambot.util.GET_HOME_LIST
 
 @Component
 class DeleteRoomHandler(
-    private val telegramSender: TelegramSender
+    private val telegramSender: TelegramSender,
+    private val roomServiceClient: RoomServiceClient,
+    private val redisRepository: RedisRepository
 ) : ActionHandler<DeleteRoomActionData> {
     override val type: ActionType = ActionType.DELETE_ROOM
 
+    @CheckAndUpdateToken
     override fun handle(data: DeleteRoomActionData) {
-
+        val roomId = data.message.split(":")[1].toInt()
+        redisRepository.findByIdOrNull(data.chatId)
+            ?.let { roomServiceClient.deleteRoom("Bearer ${it.accessToken}", roomId) }
+        telegramSender.editMessage(
+            chatId = data.chatId,
+            messageId = data.messageId,
+            text = "Вашa комната удалена",
+            inlineReplyMarkup = InlineKeyboardMarkup(listOf(listOf(
+                InlineKeyboardButton.builder()
+                .text("Вернуться назад")
+                .callbackData(GET_HOME_LIST)
+                .build())))
+        )
     }
 }
